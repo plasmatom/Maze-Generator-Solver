0f1315f72e6e25c635f4cba9d9655509e711cd00
Ahmed Ghareeb <72767396+plasmatom@users.noreply.github.com>
2024-01-06 21:58:30 +0200

    Update Maze.py

--- Maze.py
+++ Maze.py
@@ -2,7 +2,27 @@ import random
 import time
 
 class Maze:
+    """
+    Class for generating a maze and managing the starting, ending points, and path display within the maze.
+    .............
+    Methods:
+        __init__: Initializes the maze with given size, coverage, and aspect ratio.
+        generate_maze: Generates a maze based on specified coverage.
+        set_point: Sets a point in the maze based on a given value.
+        set_starting_point: Sets the starting point in the maze.
+        set_ending_point: Sets the ending point in the maze.
+        update_maze: Updates the maze based on a given position.
+        display_path: Displays a given path in the maze.
+    """
     def __init__(self, size: int, coverage: float, aspect_ratio: float):
+        """
+        Initializes the maze with given size, coverage, and aspect ratio.
+        .............
+        Input:
+            size: Size of the maze
+            coverage: Coverage ratio for maze generation
+            aspect_ratio: Aspect ratio for maze dimensions
+        """
         self.aspect_ratio = aspect_ratio
         self.height = size
         self.width = int(size * self.aspect_ratio)
@@ -12,6 +32,12 @@ class Maze:
         self.ending_point = self.set_ending_point()
 
     def generate_maze(self):
+        """
+        Generates a maze based on the specified coverage.
+        .............
+        Output:
+            Generated maze
+        """
         maze = [[0 for _ in range(self.height)] for _ in range(self.width)]
         for i in range(self.width):
             for j in range(self.height):
@@ -21,6 +47,14 @@ class Maze:
         return maze
     
     def set_point(self, value):
+        """
+        Sets a point in the maze based on a given value.
+        .............
+        Input:
+            value: Value to set in the maze
+        Output:
+            Point position
+        """
         if value == 2:
             random.seed(int(time.time()))
     
@@ -35,12 +69,30 @@ class Maze:
         return point
     
     def set_starting_point(self):
+        """
+        Sets the starting point in the maze.
+        .............
+        Output:
+            Starting point position
+        """
         return self.set_point(2)
 
     def set_ending_point(self):
+        """
+        Sets the ending point in the maze.
+        .............
+        Output:
+            Ending point position
+        """
         return self.set_point(3)
     
     def update_maze(self, postion):
+        """
+        Updates the maze based on a given position.
+        .............
+        Input:
+            position: Position to be updated
+        """
         x = postion[0]
         y = postion[1]
 
@@ -56,6 +108,12 @@ class Maze:
         
 
     def display_path(self, path):
+        """
+        Displays a given path in the maze.
+        .............
+        Input:
+            path: Path to be displayed
+        """
         try:
             for x, y in path:
                 self.maze[x][y] = 4
